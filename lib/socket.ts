9c603e87e92dcb12d7caaefa738474b2ce374160
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-02-08 16:36:37 +0530

    .env.example add

--- lib/socket.ts
+++ lib/socket.ts
@@ -4,7 +4,6 @@ import { io } from "socket.io-client";
 export const socket = io(process.env.NEXT_PUBLIC_SOCKET_API_ENDPOINT, {
   autoConnect: false,
   transports: ["websocket"],
-
   reconnection: true,
   reconnectionAttempts: 5,
   reconnectionDelay: 1000,
